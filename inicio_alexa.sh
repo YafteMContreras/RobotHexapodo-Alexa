376659f4a7e6cdf653a1ff7dadcec34dd63cff77
YafteMContreras <montielcontrerasyafte@gmail.com>
2025-09-21 20:15:57 -0600

    Actualiza bash de inicio

--- inicio_alexa.sh
+++ inicio_alexa.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
 
+echo "---------------------------------------------------------" >> /home/Cesarin/log_script.txt
 echo "Script de inicio ejecutandose el $(date)" >> /home/Cesarin/log_script.txt
 
 mensaje="Ejecutando..."
@@ -13,7 +14,8 @@ then
 	echo "Entorno virtual activado: $VIRTUAL_ENV" >> /home/Cesarin/log_script.txt
 #	lxterminal -e "echo $mensaje && bash"
 	cd /home/Cesarin/Alexa/Server
-	python3 subscriber2.py
+	python3 subscriber.py
+	echo "Programa de python ejecutandose el $(date)" >> /home/Cesarin/log_script.txt
 else
 	echo "No se pudo activar el entorno virtual"
 fi
